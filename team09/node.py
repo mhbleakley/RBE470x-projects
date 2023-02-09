d175a74c86205a5186bfcc15351efc464a013ac3
unknown <lahoy@wpi.edu>
2023-02-08 21:25:06 -0500

    reeeee

--- team09/node.py
+++ team09/node.py
@@ -3,6 +3,10 @@ class Node:
 	def __init__(self, value):
 		
 		self.value = value
+		self.dx = 0
+		self.dy = 0
+		self.mx = 0
+		self.my = 0
 		self.N = None
 		self.NE = None
 		self.E = None
@@ -14,62 +18,10 @@ class Node:
 		self.B = None #Bomb
 		self.D = None #Don't move
 	
-# Initializing Nodes to None
-def newNode(v):
-
-	temp = Node(v)
-	return temp
-
-# Getting expectimax
-def expectimax(node, is_max):
-
-	# Condition for Terminal node
-	if (node.N == None and node.NE == None and node.E == None and node.SE == None and node.S == None and node.SW == None and node.W == None and node.NW == None and node.B == None and node.D == None):
-		return node.value
-	
-	# Maximizer node. Chooses the max from the
-	# left and right sub-trees
-	if (is_max):
-		return max(expectimax(node.N, False), expectimax(node.NE, False), expectimax(node.E, False), expectimax(node.SE, False), expectimax(node.S, False), expectimax(node.SW, False), expectimax(node.W, False), expectimax(node.NW, False), expectimax(node.B, False), expectimax(node.B, False))
-
-	# Chance node. Returns the average of
-	# the left and right sub-trees
-	else:
-		return (expectimax(node.N, True), expectimax(node.NE, True), expectimax(node.E, True), expectimax(node.SE, True), expectimax(node.S, True), expectimax(node.SW, True), expectimax(node.W, True), expectimax(node.NW, True), expectimax(node.B, True), expectimax(node.B, True))/10
-	
-# def make_min(nd):
-# 	nd.N = newNode(0, 0, -1)
-# 	nd.NE = newNode(0, 1, -1)
-# 	nd.E = newNode(0, 1, 0)
-# 	nd.SE = newNode(0, 1, 1)
-# 	nd.S = newNode(0, 0, 1)
-# 	nd.SW = newNode(0, -1, 1)
-# 	nd.W = newNode(0, -1, 0)
-# 	nd.NW = newNode(0, -1, -1)
-# 	nd.B = newNode(0, 0, -1)
-# 	nd.D = newNode(0, 0, -1)
-
-def populate(node, height):
-	if height == 0:
-		node = newNode(0)
-	else:
-		node = newNode(height)
-		for i in range(0,height):
-			populate(node.N, height - 1)	
-			populate(node.NE, height - 1)	
-			populate(node.E, height - 1)	
-			populate(node.SE, height - 1)	
-			populate(node.S, height - 1)	
-			populate(node.SW, height - 1)	
-			populate(node.W, height - 1)	
-			populate(node.NW, height - 1)	
-			populate(node.B, height - 1)	
-			populate(node.D, height - 1)
-		
 # Driver code
-if __name__=='__main__':	
-	root = newNode(0)
-	populate(root, 3)
-	print(root)
-	res = expectimax(root, True)
-print("Expectimax value is "+str(res))
+# if __name__=='__main__':	
+# 	root = Node(0, 0, 0, 0, 0)
+# 	make_min(root, 5)
+# 	print(root)
+# 	res = expectimax(root, True)
+# print("Expectimax value is "+str(res))
--- team09/testcharacter.py
+++ team09/testcharacter.py
@@ -1,6 +1,7 @@
 # This is necessary to find the main code
 import sys
 import math
+from node import Node
 sys.path.insert(0, '../bomberman')
 # Import necessary stuff
 from entity import CharacterEntity
@@ -13,7 +14,6 @@ class TestCharacter(CharacterEntity):
     moves = 1
     rnge = 3
     bombCycle = 0
-    bombRecent = 100
 
     # Just looks for neighbors of 8 as the frontier
     def look_for_empty_cell(self, wrld, current):
@@ -33,50 +33,6 @@ class TestCharacter(CharacterEntity):
                                 cells.append((current[0] + dx, current[1] + dy))
         # All done
         return cells
-
-    # Looks for the empty cells around the current position, but does not allow area around monster to be considered
-    def look_for_empty_cell_monster(self, wrld, current, monster):
-        # List of empty cells
-        cells = []
-        # Go through neighboring cells
-        for dx in [-1, 0, 1]:
-            # Avoid out-of-bounds access
-            if ((current[0] + dx >= 0) and (current[0] + dx < wrld.width())):
-                for dy in [-1, 0, 1]:
-                    # Avoid out-of-bounds access
-                    if ((current[1] + dy >= 0) and (current[1] + dy < wrld.height())):
-                        # Is this cell safe?
-                        if(wrld.exit_at(current[0] + dx, current[1] + dy) or
-                           wrld.empty_at(current[0] + dx, current[1] + dy)):
-                            # Makes the area around the monster to not be considered as a frontier          
-                            for mx in [-2, -1, 0, 1, 2]:
-                                for my in [-2, -1, 0, 1, 2]:
-                                    if not ((current[0] + dx == monster[0] + mx) or
-                                        (current[1] + dy == monster[1] + my)):
-
-                                        cells.append((current[0] + dx, current[1] + dy))
-        # All done
-        return cells
-    
-    def monster_moves(self, wrld, monster, depth):
-        # List of empty cells
-        cells = []
-        # Go through neighboring cells
-        for dx in range(-depth, depth):
-            # Avoid out-of-bounds access
-            if ((monster[0] + dx >= 0) and (monster[0] + dx < wrld.width())):
-                for dy in range(-depth, depth):
-                    # Avoid out-of-bounds access
-                    if ((monster[1] + dy >= 0) and (monster[1] + dy < wrld.height())):
-                        # Is this cell safe?
-                        if not (wrld.explosion_at(monster[0] + dx, monster[1] + dy) or
-                            wrld.wall_at(monster[0] + dx, monster[1] + dy) or
-                            wrld.monsters_at(monster[0] + dx, monster[1] + dy) or
-                            wrld.exit_at(monster[0] + dx, monster[1] + dy)):
-                            cells.append((monster[0] + dx, monster[1] + dy))
-
-        # All done
-        return cells
     
     def monster_in_range(self, wrld):
         for dx in range(-self.rnge, self.rnge+1):
@@ -93,15 +49,11 @@ class TestCharacter(CharacterEntity):
     
     def heuristic(self, goal, next):
         return max(abs(next[0] - goal[0]), abs(next[1] - goal[1]))
-        # return sum(abs(val1-val2) for val1, val2 in zip(goal, next))
     
     def cost(self, wrld, current, next):
         move_cost = abs(current[0] - next[0]) + abs(current[1] - next[1])
-        if wrld.wall_at(next[0],next[1]):
-            move_cost += 16
-            for dx in [-1, 0, 1]:
-                if ((current[0] + dx >= 0) or (current[0] + dx < wrld.width())):
-                    move_cost -= 3
+        if wrld.wall_at(next[0], next[1]):
+            move_cost += 20
         return move_cost
     
     def bomb_moves(self, wrld):
@@ -120,37 +72,41 @@ class TestCharacter(CharacterEntity):
             self.move(direction,1)
         if (self.bombCycle == 6 or self.bombCycle == 4 or self.bombCycle == 3):
             self.move(0,0)
-        print(self.bombCycle)
     
     def do(self, wrld):
         found, mx, my = self.monster_in_range(wrld)
-        current_pos = self.x, self.y
-        monster = mx, my
         if found:
             self.moves = 1
-            self.path = [current_pos]
-            # print("Monster is " + str(monster))
-            new_move = self.exp_max(wrld,monster)
- 
-            self.path.append(new_move)   
-            
-            # for cell in self.path:
-            #     self.set_cell_color(cell[0],cell[1], Fore.BLACK + Back.BLACK)
+            root = Node(0)
+            self.make_min(wrld, root, 2)
+            res = self.expectimax(root, True)
+            if res == self.expectimax(root.N, True):
+                self.move(0,-1)
+            if res == self.expectimax(root.S, True):
+                self.move(0,1)
+            if res == self.expectimax(root.E, True):
+                self.move(1,0)
+            if res == self.expectimax(root.W, True):
+                self.move(-1,0)
+            if res == self.expectimax(root.NW, True):
+                self.move(-1,-1)
+            if res == self.expectimax(root.NE, True):
+                self.move(1,-1)
+            if res == self.expectimax(root.SW, True):
+                self.move(-1,1)
+            if res == self.expectimax(root.SE, True):
+                self.move(1,1)
+            if res == self.expectimax(root.D, True):
+                self.move(0,0)
         else:
             self.path = self.makePath(wrld, self.astar(wrld))
             self.moves = 1
-        # for cell in self.path:
-        #     self.set_cell_color(cell[0],cell[1], Fore.RED + Back.BLACK)
-        # print("This is the current pos " + str(current_pos))
-        # print("This is the path " + str(self.path))
-        found = False
-        
+            found = False
         if self.moves < len(self.path):
             next = self.path[self.moves]   
-            if (wrld.wall_at(next[0], next[1]) and self.bombCycle == 0 and self.bombRecent > 5):
+            if (wrld.wall_at(next[0], next[1]) and self.bombCycle == 0):
                 self.place_bomb()
                 self.bombCycle = 6
-                self.bombRecent = 0
                 self.bomb_moves(wrld)
             if (self.bombCycle != 0):
                 self.bomb_moves(wrld)
@@ -160,7 +116,6 @@ class TestCharacter(CharacterEntity):
                 dy = next[1] - self.y
                 self.move(dx,dy)
                 self.moves += 1
-            self.bombRecent +=1
         
 
     def makePath(self, wrld, came_from):
@@ -172,30 +127,6 @@ class TestCharacter(CharacterEntity):
         path.append((self.x, self.y))
         path.reverse()
         return path
-    
-    #uses the astar but with the frontier not using area around the monster as a frontier
-    def astar_monster(self, wrld, mx, my):
-        frontier = PriorityQueue()
-        came_from = {}
-        cost_so_far = {}
-        monster = (mx,my)
-
-        frontier.put((self.x, self.y), 0)
-        came_from[(self.x, self.y)] = None
-        cost_so_far[(self.x, self.y)] = 0
-        while not frontier.empty():
-            current = frontier.get()
-            goal = wrld.exitcell
-            if current == goal:
-                break
-            for next in self.look_for_empty_cell_monster(wrld, current, monster):
-                new_cost = cost_so_far[current] + 1
-                if next not in cost_so_far or new_cost < cost_so_far[next]:
-                    cost_so_far[next] = new_cost
-                    priority = new_cost + self.heuristic(goal, next)
-                    frontier.put(next, priority)
-                    came_from[next] = current
-        return came_from
     
     def astar(self, wrld):
         frontier = PriorityQueue()
@@ -218,75 +149,121 @@ class TestCharacter(CharacterEntity):
                     frontier.put(next, priority)
                     came_from[next] = current
         return came_from
-
-    def exp_max(self, wrld, monster):
-        possible_moves = self.exp_probabilities(wrld, monster)
-        best_val = -99999
-        new_move = (0,0)
-        for move in possible_moves:
-            if move[1] > best_val:
-                best_val = move[1]
-                new_move = move[0] 
-        # print("All possible moves with weight" + str(possible_moves))
-        # print("Next Move should Be" + str(new_move))
-        return new_move
-
-    def exp_probabilities(self, wrld, monster):
-        possible_moves : list[tuple(tuple, float)] = []
-        current_position = self.x, self.y
-        # Returns the possible moves that the character can do
-        character_moves = self.look_for_empty_cell(wrld, current_position)
-        # print(character_moves)
-        for character_move in character_moves:
-            end_values = self.find_values(wrld, character_move, monster)
-            chance_value = 0
-            for values in end_values:
-                chance_value += (values/len(end_values))
-            possible_moves.append((character_move, chance_value))
-
-        return possible_moves
-
-    def find_values(self, wrld, possible_move, monster):
-        # finds all possible monster moves
-        possible_monster_moves = self.monster_moves(wrld, monster, 3)
-        # created the base final values
-        end_values = []
-
-        inital_distance = self.heuristic(possible_move, monster)
-
-        for monster_move in possible_monster_moves:
-            next_distance = self.heuristic(possible_move, monster_move) 
-            # print("*****")
-            # print("OUR initial CHEBEY " + str(inital_distance))
-            # print("OUR poissible move " + str(possible_move))
-            # print("MONSTER Move " + str(monster_move))
-            # print("CHEYBEY Distance " + str(next_distance))
-            # print("****")
-
-            if next_distance == 0:
-                value = -100
-            else :
-                scale_factor = 2
-                value = scale_factor*(next_distance - inital_distance)
-
-            end_values.append(value)
-        return end_values
-
-    # def exp_max(self, wlrd, state):
-    #     return max(self.exp_val(result(state,a)))
     
-    # def exp_val(self, wrld, state):
-    #     if terminal_test:
-    #         return utility(state)
-    #     v = 0
-    #     for a in actions(state):
-    #         p = probability(a)
-    #         v = v + p*self.max_val(result(state,a))
-    #     return v
+####################################################################################################
 
-    # def max_val(self, wrld, state):
-    #     if terminal_test:
-    #         v = -9999
-    #         for a in actions(state)
-    #             v = max(v, self.exp_val(result(state,a)))
-    #         return v
+    # Initializing Nodes to None
+    def newNode(self, v, dx, dy, mx, my, nd):
+
+        temp = Node(v)
+        temp.dx = nd.dx + dx
+        temp.dy = nd.dy + dy
+        temp.mx = nd.mx + mx
+        temp.my = nd.my + my
+        return temp
+
+    # Getting expectimax
+    def expectimax(self, node, is_min):
+
+        # Condition for Terminal node
+        if (node.N == None and node.NE == None and node.E == None and node.SE == None and node.S == None and node.SW == None and node.W == None and node.NW == None and node.B == None and node.D == None):
+            return node.value
+        
+        # Minimizer node. Chooses the min from the
+        # all sub-trees
+        if (is_min):
+            return min(self.expectimax(node.N, False), self.expectimax(node.NE, False), self.expectimax(node.E, False), self.expectimax(node.SE, False), self.expectimax(node.S, False), self.expectimax(node.SW, False), self.expectimax(node.W, False), self.expectimax(node.NW, False), self.expectimax(node.B, False), self.expectimax(node.D, False))
+
+        # Chance node. Returns the average of
+        # the all sub-trees
+        else:
+            return (self.expectimax(node.N, True), self.expectimax(node.NE, True), self.expectimax(node.E, True), self.expectimax(node.SE, True), self.expectimax(node.S, True), self.expectimax(node.SW, True), self.expectimax(node.W, True), self.expectimax(node.NW, True), self.expectimax(node.D, True))/9
+        
+    def look_for_traversable(self, wrld, current):
+        # List of empty cells
+        cells = []
+        # Go through neighboring cells
+        for dx in [-1, 0, 1]:
+            # Avoid out-of-bounds access
+            if ((current[0] + dx >= 0) and (current[0] + dx < wrld.width())):
+                for dy in [-1, 0, 1]:
+                    # Avoid out-of-bounds access
+                    if ((current[1] + dy >= 0) and (current[1] + dy < wrld.height())):
+                        # Is this cell safe?
+                        if(wrld.exit_at(current[0] + dx, current[1] + dy) or
+                           wrld.empty_at(current[0] + dx, current[1] + dy)):
+                                cells.append((dx,dy))
+        # All done
+        return cells
+
+    def make_min(self, wrld, nd, depth):
+        current = (self.x,self.y)
+        safe = self.look_for_traversable(wrld,current)
+        for moves in safe:
+            if (0,-1) in moves:
+                nd.N = self.newNode(self.make_chance(wrld, nd, depth), 0, -1, 0, 0, nd)
+            if (0,1) in moves: 
+                nd.S = self.newNode(self.make_chance(wrld, nd, depth), 0, 1, 0, 0, nd)
+            if (1,0) in moves: 
+                nd.E = self.newNode(self.make_chance(wrld, nd, depth), 1, 0, 0, 0, nd)
+            if (-1,0) in moves: 
+                nd.W = self.newNode(self.make_chance(wrld, nd, depth), -1, 0, 0, 0, nd)
+            if (1,1) in moves:
+                nd.SE = self.newNode(self.make_chance(wrld, nd, depth), 1, 1, 0, 0, nd)
+            if (-1,1) in moves:
+                nd.SW = self.newNode(self.make_chance(wrld, nd, depth), -1, 1, 0, 0, nd)
+            if (1,-1) in moves:
+                nd.NE = self.newNode(self.make_chance(wrld, nd, depth), 1, -1, 0, 0, nd)
+            if (-1,-1) in moves:
+                nd.NW = self.newNode(self.make_chance(wrld, nd, depth), -1, -1, 0, 0, nd)
+            nd.B = self.newNode(self.make_chance(wrld, nd, depth), 0, 0, 0, 0, nd)
+
+    def make_chance(self, wrld, nd, depth):
+        current = (self.x,self.y)
+        safe = self.look_for_empty_cell(wrld,current)
+        if depth != 0:
+            for moves in safe:
+                if (0,-1) in moves:
+                    nd.N = self.newNode(self.make_min(wrld, nd, depth), 0, -1, 0, 0, nd)
+                if (0,1) in moves: 
+                    nd.S = self.newNode(self.make_min(wrld, nd, depth), 0, 1, 0, 0, nd)
+                if (1,0) in moves: 
+                    nd.E = self.newNode(self.make_min(wrld, nd, depth), 1, 0, 0, 0, nd)
+                if (-1,0) in moves: 
+                    nd.W = self.newNode(self.make_min(wrld, nd, depth), -1, 0, 0, 0, nd)
+                if (1,1) in moves:
+                    nd.SE = self.newNode(self.make_min(wrld, nd, depth), 1, 1, 0, 0, nd)
+                if (-1,1) in moves:
+                    nd.SW = self.newNode(self.make_min(wrld, nd, depth), -1, 1, 0, 0, nd)
+                if (1,-1) in moves:
+                    nd.NE = self.newNode(self.make_min(wrld, nd, depth), 1, -1, 0, 0, nd)
+                if (-1,-1) in moves:
+                    nd.NW = self.newNode(self.make_min(wrld, nd, depth), -1, -1, 0, 0, nd)
+                nd.D = self.newNode(self.make_min(wrld, nd, depth), 0, 0, 0, 0, nd)
+        else:
+            for moves in safe:
+                if (0,1) in moves:
+                    nd.N = self.newNode(self.value_final(wrld, nd), 0, -1, 0, 0, nd)
+                if (0,-1) in moves: 
+                    nd.S = self.newNode(self.value_final(wrld, nd), 0, 1, 0, 0, nd)
+                if (1,0) in moves: 
+                    nd.E = self.newNode(self.value_final(wrld, nd), 1, 0, 0, 0, nd)
+                if (-1,0) in moves: 
+                    nd.W = self.newNode(self.value_final(wrld, nd), -1, 0, 0, 0, nd)
+                if (1,1) in moves:
+                    nd.SE = self.newNode(self.value_final(wrld, nd), 1, 1, 0, 0, nd)
+                if (-1,1) in moves:
+                    nd.SW = self.newNode(self.value_final(wrld, nd), -1, 1, 0, 0, nd)
+                if (1,-1) in moves:
+                    nd.NE = self.newNode(self.value_final(wrld, nd), 1, -1, 0, 0, nd)
+                if (-1,-1) in moves:
+                    nd.NW = self.newNode(self.value_final(wrld, nd), -1, -1, 0, 0, nd)
+                nd.D = self.newNode(self.value_final(wrld, nd), 0, 0, 0, 0, nd)
+                    
+    def value_final(self, wrld, nd):
+        dangerValue = 0
+        goal = wrld.exit_cell
+        next = (nd.dx, nd.dy)
+        if (abs(nd.dx - nd.mx) < 3) and (abs(nd.dy - nd.my) < 3):
+            dangerValue = 1000
+        return dangerValue + self.heuristic(goal, next)
