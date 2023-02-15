681e74475eb2fed9a000832f381b8a86ce75d9bc
unknown <lahoy@wpi.edu>
2023-02-15 17:43:05 -0500

    new branch

---
+++ team09/project1/variant6.py
@@ -0,0 +1,37 @@
+# This is necessary to find the main code
+import sys
+sys.path.insert(0, '../../bomberman')
+sys.path.insert(1, '..')
+
+# Import necessary stuff
+import random
+from game import Game
+from monsters.stupid_monster import StupidMonster
+from monsters.selfpreserving_monster import SelfPreservingMonster
+
+# TODO This is your code!
+sys.path.insert(1, '../teamNN')
+from testcharacter import TestCharacter
+
+for i in range(100):
+    # Create the game
+    random.seed(i) # TODO Change this if you want different random choices
+    g = Game.fromfile('map.txt')
+    for x in range(1,7):
+        for y in range(13,15):
+            g.add_monster(SelfPreservingMonster("aggressive", # name
+                                        "A",          # avatar
+                                        x, y,        # position
+                                        1             # detection range
+            ))
+
+
+    # TODO Add your character
+    
+    g.add_character(TestCharacter("me", # name
+                                "C",  # avatar
+                                0, 0  # position
+    ))
+
+    # Run!
+    g.go(100)
