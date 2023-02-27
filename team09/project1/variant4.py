2c8e52394a0482cecce679627f4d0b5fbdfc6a48
mfigler <masonfigler@gmail.com>
2023-02-26 19:13:28 -0500

    Project 1 things

--- team09/project1/variant4.py
+++ team09/project1/variant4.py
@@ -12,7 +12,7 @@ from monsters.selfpreserving_monster import SelfPreservingMonster
 sys.path.insert(1, '../teamNN')
 from testcharacter import TestCharacter
 
-for i in range(500):
+for i in range(100):
     # Create the game
     random.seed(i) # TODO Change this if you want different random choices
     g = Game.fromfile('map.txt')
@@ -29,4 +29,4 @@ for i in range(500):
     ))
 
     # Run!
-    g.go(1)
+    g.go(500)
