fdfebced742e139a93d4a25f4c0e63eddb128184
Lahoy1015 <lahoy@wpi.edu>
2023-02-26 21:06:23 -0500

    this is a working neural network with given inputs and outputs

--- team09/layer.py
+++ team09/layer.py
@@ -1,29 +1,11 @@
-import math
 class Layer:
 
-    def __init__(self, nIn, nOut):
-        self.nIn = nIn
-        self.nOut = nOut
-        self.weights = [nIn, nOut]
-        self.biases = [nOut]
+    def __init__(self):
+        self.input = None
+        self.output = None
     
-    def makeLayer(self, nIn, nOut):
-        temp = Layer(nIn, nOut)
-        return temp
+    def forward_propagation(self, input):
+        raise NotImplementedError
 
-    def CalcOutputs(self, inputV):
-        activations = []
-        for nodeOut in range(0,self.nOut):
-            weightedInput = inputV.biases[nodeOut]
-            for nodeIn in range(0,self.nIn):
-                weightedInput += inputV[nodeIn] * inputV.weights[nodeIn, nodeOut]
-            activations[nodeOut] = self.actFunc(weightedInput)
-        return activations
-    
-    def actFunc(self, weightedInput):
-        output = 1 / (1 + math.exp(-weightedInput))
-        return output
-    
-    def nodeCost(self, outputAct, expOutput):
-        error = outputAct - expOutput
-        return error*error
+    def backward_propagation(self, output_error, learning_rate):
+        raise NotImplementedError
